5ecf8aa7d21f63cc8811b2631f1016dea089bdfd
Camilo Sabogal <lobogral@gmail.com>
2021-09-27 11:59:36 -0500

    Cambio de version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="redondeo",
-    version="0.1",
+    version="0.2",
     package_dir={'redondeo': 'src/redondeo'},
     packages=find_namespace_packages('src')
 )
