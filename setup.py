53125218ca1044216e96297195d988f3d40a0793
Camilo Sabogal <lobogral@gmail.com>
2021-09-30 16:57:25 -0500

    Agrego la funcionalidad editable con pip -e

--- setup.py
+++ setup.py
@@ -3,6 +3,5 @@ from setuptools import setup, find_namespace_packages
 setup(
     name="redondeo",
     version="0.2",
-    package_dir={'redondeo': 'src/redondeo'},
-    packages=find_namespace_packages('src')
+    packages=find_namespace_packages()
 )
