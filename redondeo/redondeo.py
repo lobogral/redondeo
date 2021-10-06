befa4a36ea2273c5bfa7331169417b2468ff2d91
Camilo Sabogal <lobogral@gmail.com>
2021-10-06 16:58:27 -0500

    Retiro tildes

--- redondeo/redondeo.py
+++ redondeo/redondeo.py
@@ -3,9 +3,9 @@ from decimal import getcontext
 from decimal import ROUND_HALF_UP
 from decimal import ROUND_HALF_EVEN
 
-def redondear(número, decimales):
+def redondear(numero, decimales):
     context = getcontext()
     context.rounding = ROUND_HALF_UP
-    redondeo = round(Decimal(str(número)), decimales)
+    redondeo = round(Decimal(str(numero)), decimales)
     context.rounding = ROUND_HALF_EVEN
     return redondeo
